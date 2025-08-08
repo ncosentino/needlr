9195ad465316e4426fe2c895633546035b14cc63
Nick Cosentino <ncosentino@gmail.com>
2025-08-07 23:14:22 -0700

    Add an AllAssembliesLoader

---
+++ src/NexusLabs.Needlr.Injection/Loaders/AllAssembliesLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace NexusLabs.Needlr.Injection.Loaders;
+
+public sealed class AllAssembliesLoader : IAssemblyLoader
+{
+    private readonly FileMatchAssemblyLoader _fileMatchAssemblyLoader;
+
+    public AllAssembliesLoader()
+    {
+        _fileMatchAssemblyLoader = new FileMatchAssemblyLoader(
+            [AppDomain.CurrentDomain.BaseDirectory],
+            fileName =>
+            {
+                return
+                    fileName.EndsWith(".exe", StringComparison.OrdinalIgnoreCase) ||
+                    fileName.EndsWith(".dll", StringComparison.OrdinalIgnoreCase);
+            });
+    }
+
+    public IReadOnlyList<Assembly> LoadAssemblies(bool continueOnAssemblyError) =>
+        _fileMatchAssemblyLoader.LoadAssemblies(continueOnAssemblyError);
+}
